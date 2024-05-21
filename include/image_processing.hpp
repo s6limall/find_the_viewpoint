e341ee2c9f14b7eb59b16c82c94347e8943862fc
Ayush <aym@protonmail.ch>
2024-05-21 20:36:41 +0200

    feat: Define image processing functions

---
+++ include/image_processing.hpp
@@ -0,0 +1,22 @@
+#ifndef IMAGE_PROCESSING_HPP
+#define IMAGE_PROCESSING_HPP
+
+#include <opencv2/opencv.hpp>
+#include <vector>
+
+const float RATIO_THRESH = 0.75f; // Define a constant for ratio test threshold
+
+cv::Mat convertToGrayscale(const cv::Mat &image);
+
+void detectAndComputeSIFT(const cv::Mat &gray, std::vector<cv::KeyPoint> &keypoints, cv::Mat &descriptors);
+
+std::vector<std::vector<cv::DMatch> > matchSIFTDescriptors(const cv::Mat &descriptors1, const cv::Mat &descriptors2);
+
+std::vector<cv::DMatch> applyRatioTest(const std::vector<std::vector<cv::DMatch> > &knnMatches,
+                                       float ratioThresh = RATIO_THRESH);
+
+bool compareImages(const cv::Mat &image1, const cv::Mat &image2);
+
+size_t computeSIFTMatches(const cv::Mat &image1, const cv::Mat &image2, float ratioThresh = RATIO_THRESH);
+
+#endif // IMAGE_PROCESSING_HPP
