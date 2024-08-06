3b7175081b4bf1b19b612f9a2a8c3ab4a68f0342
Sicong Pan <span@uni-bonn.de>
2024-08-06 18:08:54 +0200

    fix missing functions

--- reference_codes/path_and_vis.cpp
+++ reference_codes/path_and_vis.cpp
@@ -312,6 +312,135 @@ double get_trajectory_xyz(vector<Eigen::Vector3d>& points, Eigen::Vector3d M, Ei
 	return num_of_path;
 }
 
+////////// rs2_utils
+/** \brief Distortion model: defines how pixel coordinates should be mapped to sensor coordinates. */
+typedef enum rs2_distortion
+{
+	RS2_DISTORTION_NONE, /**< Rectilinear images. No distortion compensation required. */
+	RS2_DISTORTION_MODIFIED_BROWN_CONRADY, /**< Equivalent to Brown-Conrady distortion, except that tangential distortion is applied to radially distorted points */
+	RS2_DISTORTION_INVERSE_BROWN_CONRADY, /**< Equivalent to Brown-Conrady distortion, except undistorts image instead of distorting it */
+	RS2_DISTORTION_FTHETA, /**< F-Theta fish-eye distortion model */
+	RS2_DISTORTION_BROWN_CONRADY, /**< Unmodified Brown-Conrady distortion model */
+	RS2_DISTORTION_KANNALA_BRANDT4, /**< Four parameter Kannala Brandt distortion model */
+	RS2_DISTORTION_COUNT                   /**< Number of enumeration values. Not a valid input: intended to be used in for-loops. */
+} rs2_distortion;
+/** \brief Video stream intrinsics. */
+typedef struct rs2_intrinsics
+{
+	int           width;     /**< Width of the image in pixels */
+	int           height;    /**< Height of the image in pixels */
+	float         ppx;       /**< Horizontal coordinate of the principal point of the image, as a pixel offset from the left edge */
+	float         ppy;       /**< Vertical coordinate of the principal point of the image, as a pixel offset from the top edge */
+	float         fx;        /**< Focal length of the image plane, as a multiple of pixel width */
+	float         fy;        /**< Focal length of the image plane, as a multiple of pixel height */
+	rs2_distortion model;    /**< Distortion model of the image */
+	float         coeffs[5]; /**< Distortion coefficients */
+} rs2_intrinsics;
+/* Given a point in 3D space, compute the corresponding pixel coordinates in an image with no distortion or forward distortion coefficients produced by the same camera */
+static void rs2_project_point_to_pixel(float pixel[2], const struct rs2_intrinsics* intrin, const float point[3])
+{
+	float x = point[0] / point[2], y = point[1] / point[2];
+
+	if ((intrin->model == RS2_DISTORTION_MODIFIED_BROWN_CONRADY) ||
+		(intrin->model == RS2_DISTORTION_INVERSE_BROWN_CONRADY))
+	{
+
+		float r2 = x * x + y * y;
+		float f = 1 + intrin->coeffs[0] * r2 + intrin->coeffs[1] * r2 * r2 + intrin->coeffs[4] * r2 * r2 * r2;
+		x *= f;
+		y *= f;
+		float dx = x + 2 * intrin->coeffs[2] * x * y + intrin->coeffs[3] * (r2 + 2 * x * x);
+		float dy = y + 2 * intrin->coeffs[3] * x * y + intrin->coeffs[2] * (r2 + 2 * y * y);
+		x = dx;
+		y = dy;
+	}
+	if (intrin->model == RS2_DISTORTION_FTHETA)
+	{
+		float r = sqrtf(x * x + y * y);
+		if (r < FLT_EPSILON)
+		{
+			r = FLT_EPSILON;
+		}
+		float rd = (float)(1.0f / intrin->coeffs[0] * atan(2 * r * tan(intrin->coeffs[0] / 2.0f)));
+		x *= rd / r;
+		y *= rd / r;
+	}
+	if (intrin->model == RS2_DISTORTION_KANNALA_BRANDT4)
+	{
+		float r = sqrtf(x * x + y * y);
+		if (r < FLT_EPSILON)
+		{
+			r = FLT_EPSILON;
+		}
+		float theta = atan(r);
+		float theta2 = theta * theta;
+		float series = 1 + theta2 * (intrin->coeffs[0] + theta2 * (intrin->coeffs[1] + theta2 * (intrin->coeffs[2] + theta2 * intrin->coeffs[3])));
+		float rd = theta * series;
+		x *= rd / r;
+		y *= rd / r;
+	}
+
+	pixel[0] = x * intrin->fx + intrin->ppx;
+	pixel[1] = y * intrin->fy + intrin->ppy;
+}
+/* Given pixel coordinates and depth in an image with no distortion or inverse distortion coefficients, compute the corresponding point in 3D space relative to the same camera */
+static void rs2_deproject_pixel_to_point(float point[3], const struct rs2_intrinsics* intrin, const float pixel[2], float depth)
+{
+	assert(intrin->model != RS2_DISTORTION_MODIFIED_BROWN_CONRADY); // Cannot deproject from a forward-distorted image
+	//assert(intrin->model != RS2_DISTORTION_BROWN_CONRADY); // Cannot deproject to an brown conrady model
+
+	float x = (pixel[0] - intrin->ppx) / intrin->fx;
+	float y = (pixel[1] - intrin->ppy) / intrin->fy;
+	if (intrin->model == RS2_DISTORTION_INVERSE_BROWN_CONRADY)
+	{
+		float r2 = x * x + y * y;
+		float f = 1 + intrin->coeffs[0] * r2 + intrin->coeffs[1] * r2 * r2 + intrin->coeffs[4] * r2 * r2 * r2;
+		float ux = x * f + 2 * intrin->coeffs[2] * x * y + intrin->coeffs[3] * (r2 + 2 * x * x);
+		float uy = y * f + 2 * intrin->coeffs[3] * x * y + intrin->coeffs[2] * (r2 + 2 * y * y);
+		x = ux;
+		y = uy;
+	}
+	if (intrin->model == RS2_DISTORTION_KANNALA_BRANDT4)
+	{
+		float rd = sqrtf(x * x + y * y);
+		if (rd < FLT_EPSILON)
+		{
+			rd = FLT_EPSILON;
+		}
+
+		float theta = rd;
+		float theta2 = rd * rd;
+		for (int i = 0; i < 4; i++)
+		{
+			float f = theta * (1 + theta2 * (intrin->coeffs[0] + theta2 * (intrin->coeffs[1] + theta2 * (intrin->coeffs[2] + theta2 * intrin->coeffs[3])))) - rd;
+			if (abs(f) < FLT_EPSILON)
+			{
+				break;
+			}
+			float df = 1 + theta2 * (3 * intrin->coeffs[0] + theta2 * (5 * intrin->coeffs[1] + theta2 * (7 * intrin->coeffs[2] + 9 * theta2 * intrin->coeffs[3])));
+			theta -= f / df;
+			theta2 = theta * theta;
+		}
+		float r = tan(theta);
+		x *= r / rd;
+		y *= r / rd;
+	}
+	if (intrin->model == RS2_DISTORTION_FTHETA)
+	{
+		float rd = sqrtf(x * x + y * y);
+		if (rd < FLT_EPSILON)
+		{
+			rd = FLT_EPSILON;
+		}
+		float r = (float)(tan(intrin->coeffs[0] * rd) / atan(2 * tan(intrin->coeffs[0] / 2.0f)));
+		x *= r / rd;
+		y *= r / rd;
+	}
+
+	point[0] = depth * x;
+	point[1] = depth * y;
+	point[2] = depth;
+} 
 ////////// Usage of waypoints (example of visualization)
 void show_view_image_path() {
 	bool highlight_initview = true;
@@ -323,16 +452,17 @@ void show_view_image_path() {
 	// Important to modify viewspace path here.                    //
 	/////////////////////////////////////////////////////////////////
 	//Reading View Space
+	Eigen::Vector3d object_center_world = Eigen::Vector3d(1e-100, 1e-100, 1e-100);
+	double predicted_size = 1.0;
+	vector<View> views;
 	ifstream fin("./view_space/100.txt");
 	if (fin.is_open()) {
 		int num = 100;
-		object_center_world = Eigen::Vector3d(1e-10, 1e-10, 1e-10);
-		now_camera_pose_world = Eigen::Matrix4d::Identity();
-		predicted_size = 1.0;
 		for (int i = 0; i < num; i++) {
-			double init_pos[3];
-			fin >> init_pos[0] >> init_pos[1] >> init_pos[2];
-			View view(Eigen::Vector3d(init_pos[0] * 3.0, init_pos[1] * 3.0, init_pos[2] * 3.0));
+			Eigen::Vector3d positon;
+			fin >> positon[0] >> positon[1] >> positon[2];
+			View view;
+			view.compute_pose_from_positon_and_object_center(positon, object_center_world);
 			views.push_back(view);
 		}
 		cout << "viewspace readed." << endl;
@@ -376,8 +506,7 @@ void show_view_image_path() {
 	double path_color[3] = { 128, 0, 128 };
 
 	for (int i = 0; i < chosen_views.size(); i++) {
-		views[chosen_views[i]].get_next_camera_pos(now_camera_pose_world, object_center_world);
-		Eigen::Matrix4d view_pose_world = (now_camera_pose_world * views[chosen_views[i]].pose.inverse()).eval();
+		Eigen::Matrix4d view_pose_world = views[chosen_views[i]].pose_6d.eval();
 
 		double line_length = 0.3;
 
