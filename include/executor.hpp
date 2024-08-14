c28f3e6e445c9103c37fddf85a206b1dec523102
Ayush <aym@protonmail.ch>
2024-08-14 20:03:40 +0200

    feat: move includes to header

--- include/executor.hpp
+++ include/executor.hpp
@@ -14,11 +14,22 @@
 
 #include "common/io/image.hpp"
 #include "common/logging/logger.hpp"
+#include "common/utilities/camera.hpp"
+#include "common/utilities/visualizer.hpp"
 #include "core/view.hpp"
+#include "optimization/kernel/matern_52.hpp"
+#include "optimization/octree.hpp"
+#include "optimization/optimizer/gpr.hpp"
+#include "processing/image/comparison/composite_comparator.hpp"
 #include "processing/image/comparison/feature_comparator.hpp"
 #include "processing/image/comparison/ssim_comparator.hpp"
 #include "processing/image/feature/extractor.hpp"
 #include "processing/image/feature/extractor/akaze_extractor.hpp"
+#include "processing/image/feature/extractor/orb_extractor.hpp"
+#include "processing/image/feature/extractor/sift_extractor.hpp"
+#include "processing/image/feature/matcher/flann_matcher.hpp"
+#include "processing/vision/estimation/distance_estimator.hpp"
+#include "sampling/sampler/fibonacci.hpp"
 #include "types/image.hpp"
 #include "types/viewpoint.hpp"
 
@@ -47,7 +58,7 @@ private:
 
     struct Defaults {
         static constexpr std::string_view mesh_path = "./3d_models/obj_000020.ply";
-        static constexpr std::string_view target_image_path = "../../task1/target_images/obj_000020/target_2.png";
+        static constexpr std::string_view target_image_path = "./target_images/obj_000020/target_2.png";
     };
 };
 
