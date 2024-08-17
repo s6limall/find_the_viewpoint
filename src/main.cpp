1a99204a7f015945eb98e4f5033484b645d077af
Ayush <aym@protonmail.ch>
2024-08-17 15:00:37 +0200

    feat: record execution time

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,15 @@
 // File: main.cpp
 
 #include "common/logging/logger.hpp"
+#include "common/timer.hpp"
 #include "executor.hpp"
 
 int main() {
 
     try {
+        Timer timer("MAIN");
         Executor::execute();
+        timer.stop();
     } catch (const std::exception &e) {
         LOG_ERROR("An error occurred during execution: {}", e.what());
         return EXIT_FAILURE;
