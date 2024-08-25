f124bcb95506fa9828207edd8d76d74228a8a7fe
Ayush <aym@protonmail.ch>
2024-08-25 14:55:54 +0200

    fix: jsoncpp conflicts

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -5,10 +5,26 @@ find_package(OpenCV REQUIRED COMPONENTS core imgproc highgui xfeatures2d quality
 find_package(Eigen3 REQUIRED NO_MODULE)
 find_package(spdlog REQUIRED)
 find_package(yaml-cpp REQUIRED)
-find_package(jsoncpp REQUIRED)
 find_package(PCL REQUIRED COMPONENTS common io visualization)
 find_package(fmt REQUIRED)
 
+# Handle JsonCpp separately to avoid conflicts
+if(NOT TARGET JsonCpp::JsonCpp)
+    find_package(jsoncpp CONFIG QUIET)
+    if(NOT jsoncpp_FOUND)
+        find_package(PkgConfig REQUIRED)
+        pkg_check_modules(JSONCPP jsoncpp)
+        if(JSONCPP_FOUND)
+            add_library(JsonCpp::JsonCpp INTERFACE IMPORTED)
+            set_target_properties(JsonCpp::JsonCpp PROPERTIES
+                    INTERFACE_INCLUDE_DIRECTORIES "${JSONCPP_INCLUDE_DIRS}"
+                    INTERFACE_LINK_LIBRARIES "${JSONCPP_LIBRARIES}"
+            )
+        else()
+            message(FATAL_ERROR "JsonCpp not found")
+        endif()
+    endif()
+endif()
 
 # Aggregate include directories
 set(PROJECT_INCLUDE_DIRS
@@ -16,11 +32,14 @@ set(PROJECT_INCLUDE_DIRS
         ${EIGEN3_INCLUDE_DIR}
         ${spdlog_INCLUDE_DIRS}
         ${YAML_CPP_INCLUDE_DIR}
-        ${JSONCPP_INCLUDE_DIRS}
         ${PCL_INCLUDE_DIRS}
         ${fmt_INCLUDE_DIRS}
 )
 
+# Add JsonCpp include directories if found
+if(JSONCPP_INCLUDE_DIRS)
+    list(APPEND PROJECT_INCLUDE_DIRS ${JSONCPP_INCLUDE_DIRS})
+endif()
 
 # Aggregate libraries
 set(PROJECT_LIBRARIES
@@ -29,6 +48,12 @@ set(PROJECT_LIBRARIES
         Eigen3::Eigen
         spdlog::spdlog
         yaml-cpp
-        jsoncpp
         fmt::fmt
 )
+
+# Add JsonCpp to libraries
+if(TARGET JsonCpp::JsonCpp)
+    list(APPEND PROJECT_LIBRARIES JsonCpp::JsonCpp)
+elseif(JSONCPP_LIBRARIES)
+    list(APPEND PROJECT_LIBRARIES ${JSONCPP_LIBRARIES})
+endif()
